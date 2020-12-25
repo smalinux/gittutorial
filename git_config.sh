552c7f9411194f03ba5bdfd5d96608b5de4481f1
Sohaib <sohaibfacebook396@gmail.com>
2020-12-25 11:58:24 +0200

    ddd

--- git_config.sh
+++ git_config.sh
@@ -9,6 +9,9 @@ sudo yum install meld
 	git config --local diff.tool meld
 
 # aliases
+#	git unadd
+#	git g		for graphs
 # ===================
 	git config alias.l '!ls'
 	git config alias.g "log --all --decorate --oneline --graph"
+	git config --global alias.unadd 'reset HEAD --'
